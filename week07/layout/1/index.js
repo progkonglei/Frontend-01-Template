6988be2b24f069d27496c3185d1220e9d91037e4
wb-zkl01030365 <wb-zkl01030365@alibaba-inc.com>
2020-05-27 21:35:28 +0800

    提交7周作业

---
+++ week07/layout/1/index.js
@@ -0,0 +1,128 @@
+const getStyle = (element) => {
+
+    if (!element.style) {
+        element.style = {};
+    }
+
+    for (let prop in element.computedStyle) {
+        const p = element.computedStyle.value;
+        element.style[prop] = element.computedStyle[prop].value;
+
+        if (element.style[prop].toString().match(/px$/)) {
+            element.style[prop] = parseInt(element.style[prop]);
+        }
+
+        if (element.style[prop].toString().match(/^[0-9\.]+$/)) {
+            element.style[prop] = parseInt(element.style[prop]);
+        }
+    }
+
+    return element.style;
+}
+
+const layout = (element) => {
+    if (!element.computedStyle) {
+        return false;
+    }
+
+    let elementStyle = getStyle(element);
+
+    if (elementStyle.display !== 'flex') {
+        return false;
+    }
+
+    const items = element.children.filter(e => e.type === 'element');
+
+    items.sort((a, b) => {
+        return (a.order || 0) - (b.order || 0);
+    });
+
+    const style = elementStyle;
+
+    ['width', 'height'].forEach(size => {
+        if (style[size] === 'auto' || style[size] === '') {
+            style[size] = null;
+        }
+    });
+
+    if (!style.flexDirection || style.flexDirection === 'auto') {
+        style.flexDirection = 'row';
+    }
+    if (!style.alignItems || style.alignItems === 'auto') {
+        style.alignItems = 'stretch';
+    }
+    if (!style.justifyContent || style.justifyContent === 'auto') {
+        style.justifyContent = 'flex-start';
+    }
+    if (!style.flexWrap || style.flexWrap === 'auto') {
+        style.flexWrap = 'nowrap';
+    }
+    if (!style.alignContent || style.alignContent === 'auto') {
+        style.alignContent = 'stretch';
+    }
+
+    let mainSize, mainStart, mainEnd, mainSign, mainBase,
+        crossSize, crossStart, crossEnd, crossSign, crossBase;
+
+    if (style.flexDirection === 'row') {
+        mainSize = 'width';
+        mainStart = 'left';
+        mainEnd = 'right';
+        mainSign = +1;
+        mainBase = 0;
+
+        crossSize = 'height';
+        crossStart = 'top';
+        crossEnd = 'bottom';
+    }
+
+    if (style.flexDirection === 'row-reverse') {
+        mainSize = 'width';
+        mainStart = 'right';
+        mainEnd = 'left';
+        mainSign = -1;
+        mainBase = style.width;
+
+        crossSize = 'height';
+        crossStart = 'top';
+        crossEnd = 'bottom';
+    }
+
+    if (style.flexDirection === 'column') {
+        mainSize = 'height';
+        mainStart = 'top';
+        mainEnd = 'bottom';
+        mainSign = +1;
+        mainBase = 0;
+
+        crossSize = 'width';
+        crossStart = 'left';
+        crossEnd = 'right';
+    }
+
+
+    if (style.flexDirection === 'column-reverse') {
+        mainSize = 'height';
+        mainStart = 'bottom';
+        mainEnd = 'top';
+        mainSign = -1;
+        mainBase = style.width;
+
+        crossSize = 'width';
+        crossStart = 'left';
+        crossEnd = 'right';
+    }
+
+    if (style.flexDirection === 'warp-reverse') {
+        const tmp = crossStart;
+        crossStart = crossEnd;
+        crossEnd = tmp;
+        crossSign = -1
+    } else {
+        crossBase = 0;
+        crossSign = 1;
+    }
+
+}
+
+export default layout;
